1328b92361c116ed14b91a571e5e570db56e7cc8
Adrien KISSIE <fredkiss3@gmail.com>
2023-08-08 04:32:07 +0200

    ⚗️ do not use edge runtime

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 export const revalidate = 0;
-export const runtime = "edge";
+// export const runtime = "edge";
 export const fetchCache = "default-cache";
 export default function RootLayout({ children }) {
   return (
